ae814d0fa7cc2f58502e3ff8a91805c2217a391c
Chris Goosey <chris@i-expatriate.com>
2014-03-18 07:12:32 +0000

    fixed copy error in service provider

--- src/Cgoosey/ServiceGenerator/ServiceGeneratorServiceProvider.php
+++ src/Cgoosey/ServiceGenerator/ServiceGeneratorServiceProvider.php
@@ -19,27 +19,7 @@ class ServiceGeneratorServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		$this->package('cgoosey/service-generator');
-        <?php namespace Cgoosey\ServiceGenerator;
-
-use Illuminate\Support\ServiceProvider;
-
-class ServiceGeneratorServiceProvider extends ServiceProvider {
-
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
-
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $this->package('cgoosey/service-generator');
+    
         $this->app['generate.services'] = $this->app->share(function($app)
         {
             return $this->app->make('Cgoosey\ServiceGenerator\generateService');
