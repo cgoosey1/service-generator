9ded0472c7570ceb6ca3f87cd609950d03748b52
Chris Goosey <chris@i-expatriate.com>
2014-03-18 07:03:25 +0000

    Registering command in service provider

--- src/Cgoosey/ServiceGenerator/ServiceGeneratorServiceProvider.php
+++ src/Cgoosey/ServiceGenerator/ServiceGeneratorServiceProvider.php
@@ -19,7 +19,57 @@ class ServiceGeneratorServiceProvider extends ServiceProvider {
 	public function boot()
 	{
 		$this->package('cgoosey/service-generator');
-        include __DIR__.'/../../start.php';
+        <?php namespace Cgoosey\ServiceGenerator;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceGeneratorServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('cgoosey/service-generator');
+        $this->app['generate.services'] = $this->app->share(function($app)
+        {
+            return $this->app->make('Cgoosey\ServiceGenerator\generateService');
+        });
+
+        $this->commands('generate.services');
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
+
 	}
 
 	/**
--- src/start.php
+++ src/start.php
@@ -1,3 +1,3 @@
 <?php
 // Register the command with the console
-Artisan::add(new Cgoosey\ServiceGenerator\generateService());
+//Artisan::add(new Cgoosey\ServiceGenerator\generateService());
